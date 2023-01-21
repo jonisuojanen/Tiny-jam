d4b1a3da4d91be37a4f5b220eceb7b34829f6276
Deserui <70650897+jonisuojanen@users.noreply.github.com>
2023-01-22 00:31:25 +0200

    Revert "Merge branch 'main' of https://github.com/jonisuojanen/Tiny-jam"
    This reverts commit 1b0f96591fde769fea027c56681517202c845c9c, reversing
    changes made to 8d2eb7be7c3a41213fb8b615aa301449961c90e9.

--- Assets/Scripts/GameBehaviour.cs
+++ Assets/Scripts/GameBehaviour.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a352507c7c864fa77a3975a8ff8ea3bf011407fa78649e98c3ecb9bba67a6085
-size 285
+oid sha256:3faee930976cf4735415f44baf50025fd7879569989afb1070d1fe4cb552dab3
+size 2331
