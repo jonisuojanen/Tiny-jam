5bb8bfbfabfa9bfc51dbf5ddeae013406d00667b
Deserui <70650897+jonisuojanen@users.noreply.github.com>
2023-01-21 16:46:23 +0200

    TimeLeft renamed to currentTime
    currentTime is old timeLeft (Since counter is going up and not down)

--- Assets/Scripts/GameBehaviour.cs
+++ Assets/Scripts/GameBehaviour.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15a804e8c1cc47c337c29b3ba46af3e38bc1f92a1f71d663393d8e40a69dd864
-size 1753
+oid sha256:8f42cfead1704693fbba0fb874da5f4197717245a56726cef7f2eca6d50c78c5
+size 1772
