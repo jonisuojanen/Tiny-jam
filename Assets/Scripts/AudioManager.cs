33f00744741d33bbe815fe03c7237d65fef3446b
Deserui <70650897+jonisuojanen@users.noreply.github.com>
2023-01-21 13:57:44 +0200

    AudioManager Script
    Added audiomanager script / Sound Script.
    
    Created a prefab containing the script.
    -To add music you can reference the component and use Play() function.
    
    Script uses string name of the audiofile placed in AudioManager prefab in scene. (You can add them to the array)

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d630efa8bb8c96bedef74fce6158a9b9290af42c169e86f215a8897d185a07d5
+size 1054
---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ec5ecb2be41d31b63d8388071f04dc49b19043436371e267002662125a142bf1
+size 333
