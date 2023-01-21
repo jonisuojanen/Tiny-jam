4df9fe27cc80ad2a4dc07067eaa1dcbdf4690abd
tygyh <32486062+tygyh@users.noreply.github.com>
2023-01-21 22:43:46 +0100

    Rename fields and make AddMoney public

--- Assets/Scripts/Drink.cs
+++ Assets/Scripts/Drink.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f509e17603784e22cb74bbbf01d99d1506d43aef93742f16ff01901cfee5f587
-size 567
+oid sha256:6f1590c236f106d04cfc777a5d63010c6212c3c83e5d6cd9582c34b6af82b764
+size 622
--- Assets/Scripts/GameBehaviour.cs
+++ Assets/Scripts/GameBehaviour.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cfa89c6bf4399163be9a5ea643783da3d38c047c016d6f2c3dd473319e26eed5
-size 2335
+oid sha256:d58f3e09f0376e15923b09b55fff612b4c8b9e4254f2aee325c6e5e587ab9338
+size 2334
