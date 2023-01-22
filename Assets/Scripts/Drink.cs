e882ccf075cb833d34323109e27212e903986b1d
Deserui <70650897+jonisuojanen@users.noreply.github.com>
2023-01-22 11:21:41 +0200

    Animation

--- Assets/Scripts/Drink.cs
+++ Assets/Scripts/Drink.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:90fa437505432d2bb934940fe447d01ae85481e2002c1f7f2764745d6d589dce
-size 1067
+oid sha256:065c5c120be55b6cd7b5cda8ec2f81b2da6fd03e643b32b380c281bb8b3591ed
+size 1474
--- Assets/Scripts/TouchInput.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:05771897d8a05a58f9df92298941c9566acbf1f98a31e4c34a603f67e8bd137a
-size 638
