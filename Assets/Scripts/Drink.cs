78a77c80128019df9a14317aea6fab1c7c68e3c6
Deserui <70650897+jonisuojanen@users.noreply.github.com>
2023-01-22 11:51:03 +0200

    Fix

--- Assets/Scripts/Drink.cs
+++ Assets/Scripts/Drink.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9504eab21317817e51aeb9c60a5a129f337cf7b766d280061ec442983c07bc92
-size 284
+oid sha256:c69b0d34e3e0c394064870201ae16de0e4a32d2a7be3c4d27b77f913b23ff68d
+size 925
